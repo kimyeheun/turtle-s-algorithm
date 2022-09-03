685171307a2a11075b535697a3640e40409f3635
김예은 <yeheun004@gmail.com>
2022-09-03 12:38:36 +0900

    yeheun

---
+++ Judge/7th/A.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int test = sc.nextInt();
+
+        for(int i = 0; i < test; i++) {
+            long A = sc.nextLong();
+            long B  = sc.nextLong();
+            long R = A%B;
+            System.out.println(R >= 0 ? R : R+B);
+        }
+    }
+}
